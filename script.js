baec75c4274b1bb388597011070caf99c7ef1959
Pavan-Devada <115063387+Pavan-Devada@users.noreply.github.com>
2025-01-02 12:06:45 +0530

    files reordered

--- script.js
+++
@@ -1,9 +0,0 @@
-button = document.querySelector(".btn");
-
-
-button.addEventListener("pavan", (e) => {
-    console.log("trigerring with the event name", e.detail.name);
-})
-
-let newEvent = new CustomEvent("pavan", { cancelable: true, bubbles: true, detail: { name: "katna" } });
-button.dispatchEvent(newEvent);
