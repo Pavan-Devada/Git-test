549bda572000b479282e8c7e2877e796190dbde5
Pavan-Devada <115063387+Pavan-Devada@users.noreply.github.com>
2025-01-20 23:08:45 +0530

    new library project

---
+++ Library/bookInfo.js
@@ -0,0 +1,10 @@
+function allBooks(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${(this.read === "Yes") ? "already read" : "not read yet"}`);
+    }
+}
+
